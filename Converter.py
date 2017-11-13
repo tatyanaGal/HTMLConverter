dc62a46f64ba168632fba4488a16fab0747d5da2
tatyanaGal <tatyana.galitskaya@gmx.de>
2017-11-13 16:17:41 +0100

    Speicherung in einer CSV Datei - funktioniertgit add Converter.py

--- Converter.py
+++ Converter.py
@@ -1,29 +1,80 @@
 import csv
 from bs4 import BeautifulSoup
 
-# target_file=open("output.csv")
+target_file=open("output.csv")
+
+def extract_text(doc, site):
+  if site == "spiegel.de":
+    return extract_text_spiegel(doc)
+  elif site == "welt.de":
+    return extract_text_welt(doc)
+  elif site == "independent.co.uk":
+    #return extract_text_independent(doc)
+    table = doc.findAll("p")
+    for row in table:
+      print(row.text)
+
+  #else:
+   # return "unkown site"
+
+def extract_text_spiegel(doc):
+  return doc.find("div", {class_: "article-section"}).text
 
 
-#links = soup.find_all('a')
-#links = [link for link in links
- #        if link["href"].startswith("http://www.conakat.com/map/?p=")]
-#zips = soup.find_all('i')
+def extract_text_spiegel(doc):
+  return doc.find("div", {class_: "welt-content"}).text
 
-#for l, z in zip(links, zips):
- #   f.writerow((l.contents[0], z.contents[0]))
 
-with open("website_downloads_2017-11-12.csv") as filer:
+def extract_text_independent(doc):
+  return doc.find("<p>").text
+
+
+#AB HIER!!!
+with open("test.csv") as filer:
   rd = csv.reader(filer)
 
   for row in rd:
     if not row:
       continue
 
-    print(row[1])
+    url=row[1]
+    if url.startswith("http://www.independent.co.uk"):
+      site = "http://www.independent.co.uk"
+      html=row[2]
+      doc=BeautifulSoup(html, "lxml")
+
+      #text = extract_text(doc, site)
+
+      #csv.writer(target_file, [site, url, text])
+
+      #print(site)
+      #print(html)
+      #print(url)
+
+      table = doc.findAll("p")
+      
+
+      with open("output.csv","w") as target:
+        file = csv.writer(target)
+        for row in table:
+          file.writerow([site, url, row.text])
+
+target.close()
+#close("output.csv")
+
+
+#funktionierende Ausgabe von URLS:
+#with open("website_downloads_2017-11-12.csv") as filer:
+#  rd = csv.reader(filer)
+
+#  for row in rd:
+ #   if not row:
+  #    continue
+
+   # print(row[1])
     
 
-  #r = open("website_downloads_2017-11-02.csv","r")
-  #url = "http://www.independent.co.uk/news/world/americas/us-politics/jeff-sessions-russia-trump-investigation-george-papadopoulos-trouble-a8034546.html"
+#url = "http://www.independent.co.uk/news/world/americas/us-politics/jeff-sessions-russia-trump-investigation-george-papadopoulos-trouble-a8034546.html"
   #site = "independent.co.uk" # spiegel.de
 
   #html = line[0]
@@ -36,23 +87,3 @@ with open("website_downloads_2017-11-12.csv") as filer:
 #close(target_file)
 
 
-#def extract_text(doc, site):
- # if site == "spiegel.de":
-  #  return extract_text_spiegel(doc)
-  #elif site == "welt.de":
-  #  return extract_text_welt(doc)
-  #elif site == "independent.co.uk":
-   # return extract_text_independent(doc)
-  #else:
-   # return "unkown site"
-
-#def extract_text_spiegel(doc):
- # return doc.find("div", {class_: "article-section"}).text
-
-
-#def extract_text_spiegel(doc):
- # return doc.find("div", {class_: "welt-content"}).text
-
-
-#def extract_text_independent(doc):
- # return doc.find("div", {class_: "article-section"}).text
